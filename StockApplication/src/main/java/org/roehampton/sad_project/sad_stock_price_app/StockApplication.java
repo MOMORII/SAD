44b1bcc211313dd81bd21b38bdc67c37de029199
clems27 <clemohiomoje@gmail.com>
2025-03-21 21:18:53 +0000

    Idataretieval Interface added

--- StockApplication/src/main/java/org/roehampton/sad_project/sad_stock_price_app/StockApplication.java
+++ StockApplication/src/main/java/org/roehampton/sad_project/sad_stock_price_app/StockApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MainApplication extends Application {
+public class StockApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(StockApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("SAD STOCK PRICE APP!");
         stage.setScene(scene);
