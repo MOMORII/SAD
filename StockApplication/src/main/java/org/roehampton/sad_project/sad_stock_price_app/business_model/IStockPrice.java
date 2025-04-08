3d99739298d6102f9f7ebba4c85e554d03cfc99b
clems27 <clemohiomoje@gmail.com>
2025-04-08 22:41:09 +0100

    IStock price updated

--- StockApplication/src/main/java/org/roehampton/sad_project/sad_stock_price_app/business_model/IStockPrice.java
+++ StockApplication/src/main/java/org/roehampton/sad_project/sad_stock_price_app/business_model/IStockPrice.java
@@ -4,5 +4,10 @@ package org.roehampton.sad_project.sad_stock_price_app.business_model;
  * Interface for retrieving stock prices.
  */
 public interface IStockPrice {
-    double getPrice(String symbol);
+    /**
+     * Method to retrieve the current stock price for a given symbol.
+     * @param symbol Stock symbol (e.g., AAPL, TSLA, etc.)
+     * @return The current stock price.
+     */
+    double getStockPrice(String symbol);
 }
