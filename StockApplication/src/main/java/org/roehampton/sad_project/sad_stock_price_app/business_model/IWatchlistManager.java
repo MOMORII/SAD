f8abb4e781cc2601e02d6eb8c95b5ba1185d0946
clems27 <clemohiomoje@gmail.com>
2025-04-08 22:42:05 +0100

    IWatchlistManager updated

--- StockApplication/src/main/java/org/roehampton/sad_project/sad_stock_price_app/business_model/IWatchlistManager.java
+++ StockApplication/src/main/java/org/roehampton/sad_project/sad_stock_price_app/business_model/IWatchlistManager.java
@@ -1,9 +1,12 @@
 package org.roehampton.sad_project.sad_stock_price_app.business_model;
 
+import java.util.List;
+
 /**
  * Interface for managing watchlists.
  */
 public interface IWatchlistManager {
     void addStock(String symbol);
     void removeStock(String symbol);
+    List<String> getWatchlist();
 }
